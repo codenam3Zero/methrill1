db07175d2a93b3321e4943681e5f2c21806b86f4
codenam3Zero <krenjiro@gmail.com>
2021-12-01 01:03:17 +0700

    push 1

---
+++ database/factories/MemberFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Member;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MemberFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Member::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(),
+            'body' => $this->faker->paragraph(),
+            'created_by' => rand(1,10),
+        ];
+    }
+}
---
+++ resources/views/livewire/members.blade.php
@@ -0,0 +1,51 @@
+<x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        Notes
+    </h2>
+</x-slot>
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-4">
+            @if (session()->has('message'))
+                <div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md my-3" role="alert">
+                    <div class="flex">
+                        <div>
+                            <p class="text-sm">{{ session('message') }}</p>
+                        </div>
+                    </div>
+                </div>
+            @endif
+
+            <button wire:click="create()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-3">Add Notes</button>
+            
+            @if($isModal)
+                @include('livewire.create')
+            @endif
+
+            <table class="table-fixed w-full">
+                <thead>
+                    <tr class="bg-gray-100">
+                        <th class="px-4 py-2">Title</th>
+                        <th class="px-4 py-2">Body</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse($members as $row)
+                        <tr>
+                            <td class="border px-4 py-2">{{ $row->title }}</td>
+                            <td class="border px-4 py-2">{{ $row->body }}</td>
+                            <td class="border px-4 py-2">
+                                <button wire:click="edit({{ $row->id }})" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Edit</button>
+                                <button wire:click="delete({{ $row->id }})" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Hapus</button>
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td class="border px-4 py-2 text-center" colspan="5">Data tidak ditemukan.</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
